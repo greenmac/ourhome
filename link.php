6cea6d34b76be95f0f545f864ccc6458753366f9
greenmac <alwaysmac@msn.com>
2018-04-16 20:00:00 +0800

    1.上傳到ftp

--- link.php
+++ link.php
@@ -1,7 +1,7 @@
 <?php
-$link = @mysql_connect('localhost','root','curry30','ourhome');
+$link = @mysql_connect('sql301.byethost12.com','b12_21914855','mac50787','b12_21914855_ourhome');
 mysql_query("SET NAMES 'utf-8'");
-mysql_select_db('ourhome',$link);
+mysql_select_db('b12_21914855_ourhome',$link);
 if(!isset($_SESSION)){
   session_start();
 }
---
+++ link.php-本機
@@ -0,0 +1,8 @@
+<?php
+$link = @mysql_connect('localhost','root','curry30','ourhome');
+mysql_query("SET NAMES 'utf-8'");
+mysql_select_db('ourhome',$link);
+if(!isset($_SESSION)){
+  session_start();
+}
+?>
